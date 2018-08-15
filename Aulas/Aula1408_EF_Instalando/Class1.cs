006385dd5d458916f84d3ce8f69892e9e3bfb944
Jonatam <jonatamsr@hotmail.com>
2018-08-14 21:28:32 -0300

    Criação do Projeto de Instalação do EF

---
+++ Aulas/Aula1408_EF_Instalando/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1408_EF_Instalando
+{
+    /* Conceitos
+     * 
+     * 
+     * 
+     * 
+     * Instalando Entity Framework
+     * 
+     */
+
+    public class Class1
+    {
+    }
+}
