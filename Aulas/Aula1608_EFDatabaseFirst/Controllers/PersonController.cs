b403d12305f289b6383b9d8ad1f494eb5a2ab7d0
Jonatam <jonatamsr@hotmail.com>
2018-08-16 21:56:21 -0300

    Criado o ADO.NET DB First e Controller para Person

---
+++ Aulas/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1608_EFDatabaseFirst.Controllers
+{
+    class PersonController
+    {
+        void Inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
+
+        List<Person> ListarTodosPerson()
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.ToList();
+        }
+
+        Person BuscarPorId(int id)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.Find(id);
+        }
+
+        void Excluir(int id)
+        {
+            Person pExcluir = BuscarPorId(id);
+            if(pExcluir != null)
+            {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+                contexto.Person.Remove(pExcluir);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
